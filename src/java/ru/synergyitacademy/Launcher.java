30398d4503f84f8a3160e669a17cab5d1b03528e
v.ivanov <vivanov@vtb.ru>
2021-04-01 20:59:36 +0400

    add launcer class and gitignore file

---
+++ src/java/ru/synergyitacademy/Launcher.java
@@ -0,0 +1,14 @@
+package ru.synergyitacademy;
+
+public class Launcher {
+
+    /**
+     * Основная точка входа в приложение
+     *
+     * @param args список аргументов, которые можно кастомизировать
+     * @throws Exception список исключений, которые генерируются во время выполнения программы
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Привет, мир!");
+    }
+}
